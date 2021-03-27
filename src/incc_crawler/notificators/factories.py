d67e7c71a75998ee421d356d727638ec7801dc98
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-03-27 16:48:40 -0300

    refactor: use chain of resp to build messages

--- src/incc_crawler/notificators/factories.py
+++ src/incc_crawler/notificators/factories.py
@@ -1,39 +1,98 @@
 from incc_crawler import dtos
 from incc_crawler.crawler import PAGE_URL
 
+from abc import ABC, abstractmethod
+
+class BaseBuilder(ABC):
+    _next_handler: "BaseBuilder" = None
+
+    def then(self, handler: "BaseBuilder") -> "BaseBuilder":
+        self._next_handler = handler
+        return self._next_handler
+
+    def build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        msg = self._build(overview, complete, msg)
+
+        if self._next_handler:
+            return self._next_handler.build(overview, complete, msg)
+
+        return msg
+
+    @abstractmethod
+    def _build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        pass
+
+
+class HeadlineBuilder(BaseBuilder):
+    def _build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        last_period: dtos.OverviewEntry = overview.rows[0]
+        return msg + f"Período de *{last_period.period}*\n"
+
+
+class CurPeriodBuilder(BaseBuilder):
+    def _build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        last_period: dtos.OverviewEntry = overview.rows[0]
+
+        return (
+            f"{msg}"
+            f"INCC de: *{last_period.value}%* / "
+            f"acumulado no ano: *{last_period.accumulated_year}%*\n"
+        )
+
+class PrevPeriodBuilder(BaseBuilder):
+    def _build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        if len(overview.rows) < 2:
+            return msg
+
+        last_period: dtos.OverviewEntry = overview.rows[0]
+        period_before: dtos.OverviewEntry = overview.rows[1]
+        diff = self.calc_diff(last_period, period_before)
+
+        return (
+            f"{msg}"
+            f"Mês anterior: {period_before.value}% / Diferença: *{diff:.{1}f}%*\n"
+        )
+
+    def calc_diff(self, last: dtos.OverviewEntry, prev: dtos.OverviewEntry):
+        diff = last.value - prev.value
+        return diff
+
+
+class PrevYearSamePeriodBuilder(BaseBuilder):
+    def __init__(self, year_offset: int = 1):
+        self.offset = year_offset
+
+    def _build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        cur_month_idx = len(overview.rows)
+        last_year: dtos.YearlyEntry = complete.rows[self.offset]
+
+        return (
+            f"{msg}\n"
+            f"Em {last_year.year} foi *{last_year.get_month_value(cur_month_idx)}%*"
+        )
+
+class FooterDetailsBuilder(BaseBuilder):
+    def _build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        return msg + f"\n\n[Ver detalhes]({PAGE_URL})"
+
+
+class TrimmerBuilder(BaseBuilder):
+    def _build(self, overview: dtos.TableResult, complete: dtos.TableResult, msg: str) -> str:
+        return msg.replace(".", "\.")
+
 
 class MessageFactory:
     def build_message(self, overview: dtos.TableResult, complete: dtos.TableResult):
-        last_period: dtos.OverviewEntry = overview.rows[0]
-        # TODO: what if there's only 1? e.g. January
-        period_before: dtos.OverviewEntry = overview.rows[1]
-        diff = self.calc_diff_prev_diff(last_period, period_before)
+        builder = HeadlineBuilder()
+        next_builder = builder.then(CurPeriodBuilder())
+        next_builder = next_builder.then(PrevPeriodBuilder())
 
-        cur_month_idx = len(overview.rows)
-        last_year: dtos.YearlyEntry = complete.rows[1]
-        prev_year: dtos.YearlyEntry = complete.rows[2]
+        for prev_year in range(1, 5):
+            next_builder = next_builder.then(PrevYearSamePeriodBuilder(prev_year))
 
-        partial = (
-            f"Período de *{last_period.period}*\n"
+        # next_builder = next_builder.then(PrevYearSamePeriodBuilder(2))
+        # next_builder = next_builder.then(PrevYearSamePeriodBuilder(3))
+        next_builder = next_builder.then(FooterDetailsBuilder())
+        next_builder = next_builder.then(TrimmerBuilder())
 
-            # Overview last month
-            f"INCC de: *{last_period.value}%* / "
-            f"acumulado no ano: *{last_period.accumulated_year}%*\n"
-
-            # Overview prev month
-            f"Mês anterior: {period_before.value}% / Diferença: *{diff:.{1}f}%*\n"
-
-            # Last year
-            f"\nEm {last_year.year} foi *{last_year.get_month_value(cur_month_idx)}%*"
-
-            f"\nEm {prev_year.year} foi *{prev_year.get_month_value(cur_month_idx)}%*"
-
-            # Detalhes
-            f"\n\n[Ver detalhes]({PAGE_URL})"
-        )
-        return partial.replace(".", "\.")
-
-    def calc_diff_prev_diff(self, last: dtos.OverviewEntry, prev: dtos.OverviewEntry):
-        print(last.value)
-        diff = last.value - prev.value
-        return diff
+        return builder.build(overview, complete, "")
