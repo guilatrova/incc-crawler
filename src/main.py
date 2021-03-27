c6e16fb3201a7fc0ba8c36c4cd6c8b5de17706a5
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-03-27 18:07:58 -0300

    ci: add SAM template

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
+import json
 from incc_crawler import crawler
 from incc_crawler.notificators import TelegramNotificator
 
 
 telegram = TelegramNotificator()
+
+
+def lambda_handler(event, context):
+    t1, t2 = crawler.build_results()
+    telegram.send(t1, t2)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world",
+            }
+        ),
+    }
+
 
 if __name__ == "__main__":
     t1, t2 = crawler.build_results()
